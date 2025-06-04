96b6d34f5594f4814ae8d1a856a376a39532daaa
VinDicky <kevindck1610@gmail.com>
2025-06-04 18:25:58 +0700

    up

---
+++ new_streamlit_app/model_loader.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import asyncio
+import torch
+from ultralytics import YOLO
+
+def load_yolov8_model():
+    try:
+        if sys.version_info >= (3, 12):
+            try:
+                loop = asyncio.get_running_loop()
+            except RuntimeError:
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+        else:
+            asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    from torch.serialization import add_safe_globals
+    from ultralytics.nn.modules.conv import Conv, Concat
+    from ultralytics.nn.modules.block import C2f, Bottleneck, SPPF, DFL
+    from ultralytics.nn.modules.head import Detect
+    from torch.nn import BatchNorm2d
+    from torch.nn.modules.conv import Conv2d
+    from torch.nn.modules.container import ModuleList
+
+    add_safe_globals([Conv, Concat, C2f, Bottleneck, SPPF, DFL, Detect, BatchNorm2d, Conv2d, ModuleList])
+
+    model_path = os.path.join(os.path.dirname(os.path.abspath(_file_)), "Modelterbaik.pt")
+    model = YOLO(model_path)
+    return model
