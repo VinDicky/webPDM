1a217fdb8f5033a37e913407ae04dd97740934b9
VinDicky <kevindck1610@gmail.com>
2025-06-04 18:33:49 +0700

    up

--- new_streamlit_app/model_loader.py
+++ new_streamlit_app/model_loader.py
@@ -18,24 +18,21 @@ def load_yolov8_model():
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-    # === Tambahkan semua class penting yang digunakan oleh YOLOv8 ke dalam safe_globals ===
     from torch.serialization import add_safe_globals
     from ultralytics.nn.tasks import DetectionModel
     from ultralytics.nn.modules.conv import Conv, Concat
     from ultralytics.nn.modules.block import C2f, Bottleneck, SPPF, DFL
     from ultralytics.nn.modules.head import Detect
-    from torch.nn import BatchNorm2d, Sequential
+    from torch.nn import BatchNorm2d, Sequential, SiLU
     from torch.nn.modules.conv import Conv2d
     from torch.nn.modules.container import ModuleList
 
     add_safe_globals([
         DetectionModel, Conv, Concat, C2f, Bottleneck,
         SPPF, DFL, Detect, BatchNorm2d, Conv2d,
-        ModuleList, Sequential  # Tambahkan Sequential di sini
+        ModuleList, Sequential, SiLU  # ⬅ Penting!
     ])
 
-    # Pastikan __file__ digunakan dengan benar
     model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Modelterbaik.pt")
-
     model = YOLO(model_path)
     return model
