49116840801b42ba35896e745895e365971b81aa
VinDicky <kevindck1610@gmail.com>
2025-06-04 18:29:52 +0700

    up

--- new_streamlit_app/model_loader.py
+++ new_streamlit_app/model_loader.py
@@ -18,7 +18,9 @@ def load_yolov8_model():
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
+    # Tambahan agar PyTorch bisa deserialisasi custom class YOLOv8
     from torch.serialization import add_safe_globals
+    from ultralytics.nn.tasks import DetectionModel
     from ultralytics.nn.modules.conv import Conv, Concat
     from ultralytics.nn.modules.block import C2f, Bottleneck, SPPF, DFL
     from ultralytics.nn.modules.head import Detect
@@ -26,8 +28,15 @@ def load_yolov8_model():
     from torch.nn.modules.conv import Conv2d
     from torch.nn.modules.container import ModuleList
 
-    add_safe_globals([Conv, Concat, C2f, Bottleneck, SPPF, DFL, Detect, BatchNorm2d, Conv2d, ModuleList])
+    add_safe_globals([
+        DetectionModel, Conv, Concat, C2f, Bottleneck,
+        SPPF, DFL, Detect, BatchNorm2d, Conv2d, ModuleList
+    ])
 
+    # Path model
     model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Modelterbaik.pt")
+    
+    # Load model
     model = YOLO(model_path)
+
     return model
